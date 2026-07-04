fcb8bc05af0a54efe55d74b11b4dbad1392988c9
usenix17 <usenix17@starnix.net>
2026-07-03 20:55:56 -0500

    chore: move UniFi Terraform to dedicated usenix17/terraform repo
    ArgoCD does not run Terraform; it lived here only for version control.
    Now in github.com/usenix17/terraform (run via SemaphoreUI). State unchanged.

--- terraform/unifi/groups.tf
+++
@@ -1,6 +0,0 @@
-# Address groups referenced by imported policies (subnets can't be inline in a policy).
-resource "unifi_firewall_group" "freebsd_bastille" {
-  name    = "FreeBSD-Bastille"
-  type    = "address-group"
-  members = ["10.17.89.0/24"]
-}
